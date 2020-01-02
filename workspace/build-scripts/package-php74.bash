7744c0fdb3178fec6fcf7079b4befb4ab1528ca7
Tag <tagadvance+github.com@gmail.com>
2020-01-01 18:22:18 -0700

    Include php.ini in layer

--- workspace/build-scripts/package-php74.bash
+++ workspace/build-scripts/package-php74.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 mkdir --parents $WORKSPACE/lambda/layers/php74-cli/bin/
+# php binary
 cp $WORKSPACE/php74-bin/bin/php $WORKSPACE/lambda/layers/php74-cli/bin/
+# php.ini
+cp $WORKSPACE/php-src-php-*/php.ini-production $WORKSPACE/lambda/layers/php74-cli/bin/php.ini
 
 cd $WORKSPACE/lambda/layers/php74-cli
 mkdir --parents $BUILD_DIR
